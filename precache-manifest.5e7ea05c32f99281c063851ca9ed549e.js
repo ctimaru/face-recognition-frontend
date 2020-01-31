ac4bdc05735c55f2317faf1360a952ee32c4eec8
YOUR_USERNAME <c.timaru@gmail.com>
2020-01-31 20:50:21 +0100

    Updates

---
+++ precache-manifest.5e7ea05c32f99281c063851ca9ed549e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e1cabc763b5cbd4ee2069787f2c3e6",
+    "url": "/face-recognition-frontend/index.html"
+  },
+  {
+    "revision": "fa236c669c54067b131e",
+    "url": "/face-recognition-frontend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "60ab61d16a32429897ef",
+    "url": "/face-recognition-frontend/static/css/main.3aa0720f.chunk.css"
+  },
+  {
+    "revision": "fa236c669c54067b131e",
+    "url": "/face-recognition-frontend/static/js/2.d90b5409.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/face-recognition-frontend/static/js/2.d90b5409.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60ab61d16a32429897ef",
+    "url": "/face-recognition-frontend/static/js/main.91948ef0.chunk.js"
+  },
+  {
+    "revision": "1dff06e38a5890f94f1c",
+    "url": "/face-recognition-frontend/static/js/runtime-main.b867f1ce.js"
+  }
+]);
